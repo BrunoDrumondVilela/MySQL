3065ffbc5386fef8fa67226e0456adda9d152b7d
DrumondVilela <drumond770@gmail.com>
2021-12-23 11:17:56 -0300

    "Commit da tabela de colunas geradas para o GitHub"

---
+++ teste_colunas-geradas.sql
@@ -0,0 +1,58 @@
+/*markdown
+## Exemplo de Colunas Geradas:
+*/
+
+/*markdown
+### Exemplo 01)
+
+-> Apenas um exemplo simples de como a função deve ser utilizada. 
+
+
+*/
+
+USE testes;
+
+CREATE TABLE IF NOT EXISTS tbl_mult(
+    num1 SMALLINT NOT NULL,
+    num2 SMALLINT NOT NULL,
+    num3 SMALLINT GENERATED ALWAYS AS (num1*num2) VIRTUAL
+)
+
+show tables;
+
+describe tbl_mult
+
+INSERT INTO tbl_mult (num1, num2)
+VALUES (2,1), (27,38), (2,3), (2,59);
+
+SELECT * 
+FROM tbl_mult 
+
+/*markdown
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+/*markdown
+### Exemplo 02)
+
+-> Queremos criar essa tabela de modo que o campo Preco_Total seja calculado dinamicamente, multiplicando o preço do produto pela quantidade (adquirida), e aplicando um desconto percentual especificado na coluna Desconto ao preço total. 
+*/
+
+CREATE TABLE vendas(
+    id_venda SMALLINT PRIMARY KEY AUTO_INCREMENT, -- seta a coluna como chave primária da tabela
+    preco_produto decimal(6,2) NOT NULL, 
+    qtde TINYINT NOT NULL,
+    desconto DECIMAL(4,2) NOT NULL,
+    -- a coluna preco_total será feira a partir da operação proposta na criação e então aramzenada na memória 
+    preco_total DECIMAL(6,2) AS (preco_produto * qtde * (1 - desconto/100)) STORED 
+);
+
+INSERT INTO vendas (preco_produto, qtde, desconto)
+VALUES 
+    (50.00, 2, 20),
+    (65.35, 3, 15),
+    (100.00, 1, 12),
+    (132.00, 3, 18);
+
+SELECT * 
+FROM vendas
